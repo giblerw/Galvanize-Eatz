f0f1e9245181633d8f91ff46c1aec780fc074443
Weston Gibler <westongibler@gmail.com>
2017-10-11 07:26:42 -0600

    Added menu prices from API

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,38 +1,54 @@
-// function menuCreation(menu){
-//     var foodTitle = document.getElementsByClassName('hb');
-//     // var nameElement = foodTitle.createElement("span");
-//     // var foodPrive = document.getElementsByClassName('priceSpot')
-//     // var priceElement = foodPrice[0].createElement("div");
-//
-//     foodTitle.className += "item-name";
-// var titleHead = menuArray['0'].name;
-// console.log(titleHead);
-//     // nameElement.innerHTML = menu.menu[0];
-//
-//     // priceElement.className = "item-price";
-//     // priceElement.innerHTML = item.price;
-//
-//     foodTitle.appendChild(nameElement);
-//     // divElement.appendChild(priceElement);
-//
-//     // return divElement;
-// }
-//
-// window.addEventListener('load', menuCreation(), false);
-// function createMenuItems(menu){
-//     var menuItems = [];
-//     for (var i = 0; i < menu.length; i++){
-//         menuItems.push(new MenuItem(menu[i]));
-//     }
-//
-//     return menuItems;
-// }
-//
-//
-// function insertMenu(menu){
-//
-//         }
-//
-//
-//
-// // highlightItem(itemSelector.find(".item")[0]);
+const getURL = 'https://galvanize-eats-api.herokuapp.com/menu';
+const postURL = 'https://galvanize-eats-api.herokuapp.com/orders';
+
+function connectAPI() {
+fetch(getURL)
+  .then(function(response) {
+    return response.json()
+    .then(function(e) {
+      var menuArray = e.menu
+
+// Create Menu Items
+function menuCreation(menu){
+    var foodTitleHB = document.getElementById('hb');
+    var foodTitleCB = document.getElementById('cb');
+    var foodTitleCP = document.getElementById('cp');
+    var foodTitlePP = document.getElementById('pp');
+    var foodTitleSP = document.getElementById('sp');
+
+    var foodPriceHB = document.getElementById('priceHB');
+    var foodPriceCB = document.getElementById('priceCB');
+    var foodPriceCP = document.getElementById('priceCP');
+    var foodPricePP = document.getElementById('pricePP');
+    var foodPriceSP = document.getElementById('priceSP');
+//Hamburger
+      var titleHeadHB = menuArray['2'].name;
+          foodTitleHB.innerHTML = titleHeadHB
+        var phb = menuArray['2'].price;
+          foodPriceHB.innerHTML = `$` + phb;
+//Cheesebuger
+      var titleHeadCB = menuArray['0'].name;
+          foodTitleCB.innerHTML = titleHeadCB
+        var pcb = menuArray['0'].price;
+          foodPriceCB.innerHTML = `$` + pcb
+//Cheese Pizza
+      var titleHeadCP = menuArray['1'].name;
+          foodTitleCP.innerHTML = titleHeadCP
+        var pcp = menuArray['1'].price;
+          foodPriceCP.innerHTML = `$` + pcp
+//Pepperoni Pizza
+      var titleHeadPP = menuArray['3'].name;
+          foodTitlePP.innerHTML = titleHeadPP
+        var ppp = menuArray['3'].price;
+          foodPricePP.innerHTML = `$` + ppp
+//Sausage Pizza
+      var titleHeadSP = menuArray['4'].name;
+          foodTitleSP.innerHTML = titleHeadSP
+        var psp = menuArray['4'].price;
+          foodPriceSP.innerHTML = `$` + psp
+      }
+window.addEventListener('load', menuCreation(), false);
+    })
+  })
+}
+window.addEventListener('load', connectAPI(), false);
